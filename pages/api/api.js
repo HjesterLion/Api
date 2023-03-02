0ec58ba48170622c1310a1ef9c9571e44cee6657
HJesterLion <lucasnakazima@outlook.com.br>
2023-03-02 15:21:05 -0300

    post corrigido

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,12 +1,15 @@
 function api(req,res){
     console.log(req.body)
     if(req.method == 'POST'){
+        console.log('caiu')
         res.json({
             date:'aaa'
         })
 
+    }else{
+
+        res.send('Ha')
     }
-    res.send('Ha')
 }
 
 module.exports = api
