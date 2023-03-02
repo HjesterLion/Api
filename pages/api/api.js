6640b1cb3c5056bec95be09f55b055ba81f73eb8
HJesterLion <lucasnakazima@outlook.com.br>
2023-03-02 16:58:58 -0300

    Adição de config e concerto de bug

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,18 +1,17 @@
-function api(req,res){
-    console.log(req.body)
-    console.log(process.env.APP_KEY)
-    if(req.method == 'POST' && req.body.app_key == process.env.APP_KEY){
+function api(req, res) {
+    console.log(req.method)
+    // console.log(process.env.APP_KEY)
+    if (req.method == 'POST' && req.body.app_key == process.env.APP_KEY) {
         console.log('caiu')
-        res.json({
-            auth:'autorizado',
+        return res.send({
+            auth: 'autorizado',
             status: 200,
             data: req.body,
 
         })
 
-    }else{
-
-        res.json({status:200, msg:"Entrada não autorizada"})
+    } else {
+        return res.json({ status: 200, msg: "Entrada não autorizada" })
     }
 }
 
